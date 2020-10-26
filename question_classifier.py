eb19697301985e4c0d2e2880664da44d7bea1d58
Mike Madeira <michael-madeira@hotmail.com>
2020-10-26 02:00:24 +0000

    First Classification Results

--- question_classifier.py
+++ question_classifier.py
@@ -10,6 +10,8 @@ import nltk
 import random
 import pickle
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
 from string import punctuation
 from nltk.corpus import stopwords
@@ -23,7 +25,8 @@ from sklearn.pipeline import Pipeline
 
 from sklearn.model_selection import train_test_split
 from nltk.classify.scikitlearn import SklearnClassifier
-from sklearn import model_selection, preprocessing, linear_model, naive_bayes, metrics, svm
+from sklearn import model_selection, preprocessing, linear_model, metrics, svm
+from sklearn.naive_bayes import GaussianNB, MultinomialNB, BernoulliNB
 
 
 def main():
@@ -33,34 +36,56 @@ def main():
     filename = args[2]
     #testfile = args[3]
 
-    questions = load_dataset(filename)
-    # df = create_dataframe(labels, questions)
-    # df.sort_values(by=['label'], inplace=True)
+    questions = extract_questions(filename)
+    print("Number of Questions",np.shape(questions))
+    # print([question for question in questions if 'funnel' in question])
 
-    # print(df[:20])
+    words = extract_allwords(filename)
 
     stop_words = set(["´´", "``", "the", "in", "of", "is", "a", "s", "'s", "d", "ll", "m", "o", "re", "ve", "y", "n't"] + list(punctuation))
 
     question_categories = set(["how", "who", "when", "where", "what", "which"])
 
+    ### Maybe because of set() the order of the vocabulary is always reordered when running the code
     vocabulary, coarse_labels, fine_labels = pre_processing(filename, stop_words)
 
+    labels = coarse_labels
+    if flag == 'fine':
+        labels = fine_labels
+        
     # grammatical_pattern_features(questions_processed, question_categories)
 
-    tf_idf_matrix = tf_idf(questions, vocabulary)
-
-    train_x, valid_x, train_y, valid_y = train_test_split(tf_idf_matrix, coarse_labels, random_state=42, test_size=0.2, shuffle=True)
-
-    # SVM on Ngram Level TF IDF Vectors
-    accuracy = train_model(svm.SVC(), train_x, train_y, valid_x, valid_y)
-    print("SVM, N-Gram Vectors: ", accuracy)
-
+    tf_idf_matrix = tf_idf(questions, words)
+    tf_idf_matrix_processed = tf_idf(questions, vocabulary)
     
+    words_df = create_dataframe(words, tf_idf_matrix)
+    vocabulary_df = create_dataframe(vocabulary, tf_idf_matrix_processed)
+    print(vocabulary_df.shape)
 
-    
+    # Remove less common words that doesn't give too much information
+    common_words = [column for column in vocabulary_df.columns if vocabulary_df[column].sum() > 0.2]
+    vocabulary_df = vocabulary_df[common_words]
+
+    print(vocabulary_df.shape)
+
+    vocabularies = [words_df, vocabulary_df]
+    classifiers = {'Gaussian':GaussianNB(), 'Multinomial':MultinomialNB(), 'SVM':svm.SVC()}
+
+    accuracies={'Gaussian':[], 'Multinomial':[], 'SVM':[]}
+    for classifier in classifiers.keys():
+        for vocabulary in vocabularies:
+            accuracies[classifier].append(classification_results(classifiers[classifier], vocabulary, labels))
+        
+    plot_classification_results(accuracies.keys(), accuracies)
+
+    ### TO DO
+        # - Add n-grams to features
+        # - Add headwords to features
+        # - Add question category to features
+        # - Compare results between classifiers
 
 
-def load_dataset(filename):
+def extract_questions(filename):
 
     data = open('data/{}'.format(filename)).read()
     
@@ -76,6 +101,16 @@ def load_dataset(filename):
 
     return questions
 
+def extract_allwords(filename):
+
+    with open('data/{}'.format(filename), "r") as r:
+        vocabulary = []
+        for line in r.readlines():
+            words = nltk.word_tokenize(line)
+            for word in words[3:]:
+                vocabulary.append(word.lower())
+    return set(vocabulary)
+
 
 def get_wordnet_pos(tag):
     if tag.startswith('J'):
@@ -85,8 +120,9 @@ def get_wordnet_pos(tag):
     elif tag.startswith('R'):
         return wordnet.ADV
     return wordnet.NOUN
+            
 
-
+    return set(vocabulary)
 def pre_processing(filename, stop_words):
     with open('data/{}'.format(filename), "r") as r:
         vocabulary = []
@@ -128,11 +164,10 @@ def tf_idf(corpus, vocabulary):
                  ('tfid', TfidfTransformer())]).fit(corpus)
     return pipe.transform(corpus).toarray() #tfidf_vect_ngram.transform(vocabulary)
 
-def create_dataframe(questions, labels):
+def create_dataframe(vocabulary, tf_idf_vocabulary):
 
-    df = pd.DataFrame()
-    df['question'] = questions
-    df['label'] = label_encoding(labels)
+    df = pd.DataFrame(tf_idf_vocabulary)
+    df.columns = vocabulary
     return df
 
 def train_model(classifier, feature_vector_train, label, feature_vector_valid, valid_y, is_neural_net=False):
@@ -142,10 +177,46 @@ def train_model(classifier, feature_vector_train, label, feature_vector_valid, v
     # predict the labels on validation dataset
     predictions = classifier.predict(feature_vector_valid)
     
-    if is_neural_net:
-        predictions = predictions.argmax(axis=-1)
-    
     return metrics.accuracy_score(predictions, valid_y)
+
+def classification_results(classifier, X, y):
+
+    train_x, valid_x, train_y, valid_y = train_test_split(X, y, random_state=42, test_size=0.2, shuffle=True)
+    # SVM on Ngram Level TF IDF Vectors
+    accuracy = train_model(classifier, train_x, train_y, valid_x, valid_y)
+
+    return accuracy
+
+def set_axes(xvalues: list, ax: plt.Axes = None, title: str = '', xlabel: str = '', ylabel: str = '', xrotation: int = 0,percentage=False):
+    if ax is None:
+        ax = plt.gca()
+    ax.set_title(title)
+    ax.set_xlabel(xlabel)
+    ax.set_ylabel(ylabel)
+    if percentage:
+        ax.set_ylim(0.0, 1.0)
+    ax.set_xlabel(xlabel)
+    ax.set_ylabel(ylabel)
+    ax.set_xticklabels(xvalues, rotation=xrotation, fontsize='small', ha='center')
+
+    return ax
+
+def plot_classification_results(classifiers, accuracies):
+
+    fig, axs = plt.subplots(1, 2, figsize=(12, 8))
+    titles = ['Classify Dev Questions (without pre-processing)', 'Classify Dev Questions (with pre-processing)']
+    for i in range(2):
+        axs[i] = set_axes(classifiers, ax=axs[i], title=titles[i], xlabel='Classifiers', ylabel='Accuracies', percentage=True)
+
+        x = 1  # the label locations
+        colors = {'Gaussian':'blue', 'Multinomial':'orange', 'SVM':'green'}
+        for classifier in classifiers:
+            axs[i].bar(x, accuracies[classifier][i], width=0.4, color=colors[classifier],  align='center', label=classifier)
+            x += 1
+        axs[i].set_xticks(np.arange(len(classifiers))+1)
+        axs[i].legend(fontsize='x-small', title_fontsize='small')
+    
+    plt.show()
 
 
 if __name__ == '__main__':
---
+++ test.py
@@ -0,0 +1,28 @@
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.pipeline import Pipeline
+import numpy as np
+import pandas as pd
+
+corpus = ['this is the first document',
+          'this document is the second document',
+          'and this is the third one',
+          'is this the first document']
+vocabulary = ['this', 'document', 'first', 'is', 'second', 'the',
+              'and', 'one']
+pipe = Pipeline([('count', CountVectorizer(vocabulary=vocabulary)),
+                 ('tfid', TfidfTransformer())]).fit(corpus)
+
+print(pipe['count'].transform(corpus).toarray())
+
+print(pipe['tfid'].idf_)
+
+tfidf = pipe.transform(corpus).toarray()
+print(tfidf)
+
+df = pd.DataFrame(tfidf)
+df.columns = vocabulary
+
+# print(df.append(df.sum(axis=0), ignore_index=True) )
+
+print([remove_column for remove_column in df.columns if df[remove_column].sum() < 0.6])
